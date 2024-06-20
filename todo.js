8b2c8d8f09e6a4eb5f5e4c1ba5cccc1e2ec98c8a
yermxx <66yrlee99@gmail.com>
2024-06-21 02:51:11 +0900

    🔨 Add all select and all delete

--- todo.js
+++ todo.js
@@ -5,6 +5,8 @@ const todoInput = document.querySelector('.list-input');
 const submitBtn = document.querySelector('.submit-btn');
 const todoList = document.getElementById('todo-list');
 const rabbitImage = document.querySelector('.rabbit');
+const allCheck = document.getElementById('all-check');
+const allDelete = document.getElementById('all-delete');
 
 function updateRabbitImage() {
   if (todoList.getElementsByTagName('li').length > 0) {
@@ -14,25 +16,32 @@ function updateRabbitImage() {
   }
 }
 
+function areAllChecked() {
+  const checkboxes = document.querySelectorAll('.checkbox');
+  return Array.from(checkboxes).every(checkbox => checkbox.dataset.checked === 'true');
+}
 
 submitBtn.addEventListener('click', function(event) {
 
   event.preventDefault();
 
-  if (todoInput !== '') {
+  if (todoInput.value !== '') {
   const li = document.createElement("li");
 
   const checkBox = document.createElement("img");
   checkBox.src = './assets/check.png';
   checkBox.alt = 'checkbox';
   checkBox.classList.add('checkbox');
+  checkBox.dataset.checked = 'false'
   checkBox.addEventListener('click', function() {
-    if (checkBox.src.includes('check.png')) {
+    if (checkBox.dataset.checked === 'false') {
         checkBox.src = './assets/check-focus.png';
+        checkBox.dataset.checked = 'true';
     } else {
         checkBox.src = './assets/check.png';
+        checkBox.dataset.checked = 'false';
     }
-});
+  });
 
   const span = document.createElement('span');
   span.textContent = todoInput.value;
@@ -63,10 +72,31 @@ submitBtn.addEventListener('click', function(event) {
 
   updateRabbitImage();
   }
+  });
+
+  allCheck.addEventListener('click', function(event) {
+    event.preventDefault();
+    const checkboxes = document.querySelectorAll('.checkbox');
+    checkboxes.forEach((checkbox) => {
+      checkbox.click();
+    });
+  });
+
+  allDelete.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (areAllChecked()) {
+      const todos = todoList.querySelectorAll('li');
+      todos.forEach((li) => {
+          todoList.removeChild(li);
+      });
+      updateRabbitImage();
+    } else {
+     alert('전체 선택 후 삭제 가능');
+    }
+  });
 
   updateRabbitImage();
-
-  });
 });
+
 
 
