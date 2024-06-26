0b6613fa7c81247c0e1bd6316723dcea418f7072
yermxx <66yrlee99@gmail.com>
2024-06-26 18:03:39 +0900

    🔨 Add all-check state management

--- todo.js
+++ todo.js
@@ -1,82 +1,97 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const todoInput = document.querySelector('.list-input');
-const submitBtn = document.querySelector('.submit-btn');
-const todoList = document.getElementById('todo-list');
+  const todoInput = document.querySelector('.list-input');
+  const submitBtn = document.querySelector('.submit-btn');
+  const todoList = document.getElementById('todo-list');
 
-const rabbitImage = document.querySelector('.rabbit');
+  const tokkiImage = document.querySelector('.tokki');
 
-const allCheck = document.getElementById('all-check');
-const allCheckImage = allCheck.querySelector('img');
-const allDelete = document.getElementById('all-delete');
-const allDeleteImage = allDelete.querySelector('img');
+  const allCheck = document.getElementById('all-check');
+  const allCheckImage = allCheck.querySelector('img');
+  const allDelete = document.getElementById('all-delete');
+  const allDeleteImage = allDelete.querySelector('img');
 
-/* 타이틀 토끼 이미지 변경 */
-function updateRabbitImage() {
-  rabbitImage.style.backgroundImage = todoList.getElementsByTagName('li').length > 0
-    ? "url('./assets/awake-tokki.png')"
-    : "url('./assets/sleeping-tokki.png')";
-}
+  /* ADD 버튼 click 시 이벤트 실행 */
+  submitBtn.addEventListener('click', function(event) {
 
-/* 전체 선택 요소 상태 확인 */
-function areAllChecked() {
-  const checkboxes = document.querySelectorAll('.checkbox');
-  return Array.from(checkboxes).every(checkbox => checkbox.dataset.checked === 'true');
-}
+    event.preventDefault();
 
-/* ADD 버튼 click 시 이벤트 실행 */
-submitBtn.addEventListener('click', function(event) {
+    if (todoInput.value !== '') {
+    const li = document.createElement('li');
 
-  event.preventDefault();
+    const checkBox = document.createElement('img');
+    checkBox.src = './assets/check.png';
+    checkBox.alt = 'checkbox';
+    checkBox.classList.add('checkbox');
+    checkBox.dataset.checked = 'false'
+    checkBox.addEventListener('click', function() {
+      if (checkBox.dataset.checked === 'false') {
+          checkBox.src = './assets/check-focus.png';
+          checkBox.dataset.checked = 'true';
+      } else {
+          checkBox.src = './assets/check.png';
+          checkBox.dataset.checked = 'false';
+      }
+    });
 
-  if (todoInput.value !== '') {
-  const li = document.createElement("li");
-
-  const checkBox = document.createElement("img");
-  checkBox.src = './assets/check.png';
-  checkBox.alt = 'checkbox';
-  checkBox.classList.add('checkbox');
-  checkBox.dataset.checked = 'false'
-  checkBox.addEventListener('click', function() {
-    if (checkBox.dataset.checked === 'false') {
-        checkBox.src = './assets/check-focus.png';
-        checkBox.dataset.checked = 'true';
-    } else {
-        checkBox.src = './assets/check.png';
-        checkBox.dataset.checked = 'false';
+    /* 토끼 이미지 상태 업데이트 */
+    function updateTokkiImage() {
+      tokkiImage.style.backgroundImage = todoList.getElementsByTagName('li').length > 0
+        ? "url('./assets/awake-tokki.png')"
+        : "url('./assets/sleeping-tokki.png')";
     }
-  });
 
-  const span = document.createElement('span');
-  span.textContent = todoInput.value;
-  span.classList.add('todo-text');
+    const span = document.createElement('span');
+    span.textContent = todoInput.value;
+    span.classList.add('todo-text');
 
-  const deleteIcon = document.createElement('img');
-  deleteIcon.src = './assets/delete-icon.png';
-  deleteIcon.alt = 'Delete';
-  deleteIcon.classList.add('delete-icon');
-  deleteIcon.addEventListener('click', function() {
-    todoList.removeChild(li);
-    updateRabbitImage();
-  });
-
-  deleteIcon.addEventListener('mouseover', () => {
-    deleteIcon.src = './assets/delete-icon-green.png';
-  });
-  deleteIcon.addEventListener('mouseout', () => {
+    const deleteIcon = document.createElement('img');
     deleteIcon.src = './assets/delete-icon.png';
-  });
+    deleteIcon.alt = 'Delete';
+    deleteIcon.classList.add('delete-icon');
+    deleteIcon.addEventListener('click', function() {
+      todoList.removeChild(li);
+      updateTokkiImage();
+      updateAllCheckState();
+    });
 
-  li.appendChild(checkBox);
-  li.appendChild(span);
-  li.appendChild(deleteIcon);
+    deleteIcon.addEventListener('mouseover', () => {
+      deleteIcon.src = './assets/delete-icon-green.png';
+    });
+    deleteIcon.addEventListener('mouseout', () => {
+      deleteIcon.src = './assets/delete-icon.png';
+    });
 
-  todoList.appendChild(li);
-  todoInput.value= "";
+    li.appendChild(checkBox);
+    li.appendChild(span);
+    li.appendChild(deleteIcon);
 
-  updateRabbitImage();
+    todoList.appendChild(li);
+    todoInput.value= '';
+
+    updateTokkiImage();
+    updateAllCheckState();
+    }
+    });
+
+  /* 전체 선택 요소 상태 확인 */
+  function areAllChecked() {
+    const checkboxes = document.querySelectorAll('.checkbox');
+    return Array.from(checkboxes).every(checkbox => checkbox.dataset.checked === 'true');
   }
-  });
+
+  /* 전체 선택 상태 업데이트 */
+  function updateAllCheckState() {
+    if (areAllChecked()) {
+      allCheck.style.color = '#ACCF61';
+      allCheckImage.src = './assets/all-check-green.png';
+      allCheck.dataset.checked = 'true';
+    } else {
+      allCheck.style.color = '';
+      allCheckImage.src = './assets/all-check.png';
+      allCheck.dataset.checked = 'false';
+    }
+  }
 
   /* 전체 선택 click 이벤트 */
   allCheck.dataset.checked = 'false';
@@ -84,19 +99,24 @@ submitBtn.addEventListener('click', function(event) {
     event.preventDefault();
 
     const checkboxes = document.querySelectorAll('.checkbox');
-    checkboxes.forEach((checkbox) => {
-      checkbox.click();
-    });
 
     if (allCheck.dataset.checked === 'false') {
+      checkboxes.forEach((checkbox) => {
+        checkbox.src = './assets/check-focus.png';
+        checkbox.dataset.checked = 'true';
+      });
       allCheck.style.color = '#ACCF61';
       allCheckImage.src = './assets/all-check-green.png';
       allCheck.dataset.checked = 'true';
-  } else {
+    } else {
+      checkboxes.forEach((checkbox) => {
+        checkbox.src = './assets/check.png';
+        checkbox.dataset.checked = 'false';
+      });
       allCheck.style.color = '';
       allCheckImage.src = './assets/all-check.png';
       allCheck.dataset.checked = 'false';
-  }
+    }
   });
 
   /* 전체 삭제 click 이벤트 */
@@ -108,14 +128,16 @@ submitBtn.addEventListener('click', function(event) {
       allDeleteImage.src = './assets/all-delete-green.png';
       const toDos = todoList.querySelectorAll('li');
       toDos.forEach((li) => {
-          todoList.removeChild(li);
+        todoList.removeChild(li);
       });
       location.reload();
-      updateRabbitImage();
+      updateTokkiImage();
+      updateAllCheckState();
     } else {
       alert('전체 삭제를 위해서는 먼저 전체 선택을 해주세요!');
     }
   });
 
-  updateRabbitImage();
+  updateTokkiImage();
+  updateAllCheckState();
 });
